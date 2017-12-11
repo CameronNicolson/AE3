542563b6a4ffff347854cd7dfedf7971ead78053
CameronNicolson <cameronnicolson50@gmail.com>
2017-12-11 18:43:55 +0000

    Added method to insert FitnessClass

--- FitnessProgram.java
+++ FitnessProgram.java
@@ -7,5 +7,27 @@ import java.util.*;
  * In addition an array can be returned in order of average attendance
  */
 public class FitnessProgram {
-    // your code here
+	private FitnessClass [] programArr;
+	final int maxNoClasses = 7;
+	private int noClassesAdded; 
+    
+    FitnessProgram() {
+        this.programArr = new FitnessClass[7];
+
+        for(int x = 0; x < maxNoClasses; x++) {
+            this.programArr[x] = null;
+            System.err.println(this.programArr[x]);
+        }
+
+    }
+
+    public void insertFitnessClass(FitnessClass fitClass) {
+        int x = fitClass.getStartTime() - 9;
+        if(this.programArr[x] == null ) {
+             System.out.printf("OK ADDED: %s %s %s pos %s %n", fitClass.getId(), fitClass.getTutor(), fitClass.getClassname(), x);
+             this.programArr[x] = fitClass;
+             noClassesAdded++;
+        }
+       
+    }
 }
