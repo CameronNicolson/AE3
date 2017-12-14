7854db8eadbd7efe6d1dcb7aaf8a9bbdd70b3f70
CameronNicolson <cameronnicolson50@gmail.com>
2017-12-14 14:34:50 +0000

    Creates a new JFrame object with JTextArea

--- ReportFrame.java
+++ ReportFrame.java
@@ -5,5 +5,18 @@ import javax.swing.*;
  * Class to define window in which attendance report is displayed.
  */
 public class ReportFrame extends JFrame {
-        // your code here
+
+		/** Display of class timetable */
+		private JTextArea display;
+
+        ReportFrame() {
+	        this.setTitle("Attendance Reportffff");
+		    this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+			this.display = new JTextArea();
+			this.display.setFont(new Font("Monospaced", Font.PLAIN, 14));
+			this.getContentPane().add(display, BorderLayout.CENTER);
+			this.setSize(700, 300);
+			this.setLocationRelativeTo(null);
+			this.setVisible(true);
+        }
 }
