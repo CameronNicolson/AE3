b6d82b7bcf0b34f442d03def6ed47d72f327a2cb
CameronNicolson <cameronnicolson50@gmail.com>
2017-12-16 00:57:54 +0000

    Renamed variable and added comments

--- ReportFrame.java
+++ ReportFrame.java
@@ -2,19 +2,19 @@ import java.awt.*;
 import javax.swing.*;
 
 /**
- * Class to define window in which attendance report is displayed.
+ * Class to define window in which attendance report is reported.
  */
 public class ReportFrame extends JFrame {
 
-		/** Display of class timetable */
-		private JTextArea display;
+		/** report of report window */
+		private JTextArea report;
 
         ReportFrame() {
-	        this.setTitle("Attendance Reportffff");
+	        this.setTitle("Attendance Report");
 		    this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-			this.display = new JTextArea();
-			this.display.setFont(new Font("Monospaced", Font.PLAIN, 14));
-			this.getContentPane().add(display, BorderLayout.CENTER);
+			this.report = new JTextArea();
+			this.report.setFont(new Font("Monospaced", Font.PLAIN, 14));
+			this.getContentPane().add(report, BorderLayout.CENTER);
 			this.setSize(700, 300);
 			this.setLocationRelativeTo(null);
 			this.setVisible(true);
